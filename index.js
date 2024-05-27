f85e792720bcd457cde9d547a16ca642dd94e6f6
heyjoshlee <lee45276@gmail.com>
2024-05-27 13:12:54 +0800

    Connect Mongo

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ app.use(cors());
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
+const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}.`)
-})
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}.`)
+  }))
   .catch((error) => console.log(error.message)); 
